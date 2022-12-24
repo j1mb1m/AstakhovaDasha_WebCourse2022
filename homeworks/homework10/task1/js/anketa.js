32888f0da7fd7317e122e35e117b76e256286c88
Dasha Astakhova <dasha.kozhalo@gmail.com>
2022-12-24 09:29:38 +0300

    changed js

--- homeworks/homework10/task1/js/anketa.js
+++ homeworks/homework10/task1/js/anketa.js
@@ -5,10 +5,12 @@ var patronymic = prompt('Отчество?');
 var age = parseInt(prompt('Сколько вам лет?', 18));
 var gender = confirm('Bаш пол - мужской??');
 
-alert('ваше ФИО: ' + lastName.trim() + ' ' + firstName.trim() + ' ' + patronymic.trim() + '\n'
+var information = 'ваше ФИО: ' + lastName.trim() + ' ' + firstName.trim() + ' ' + patronymic.trim() + '\n'
 + 'ваш возраст в годах: ' + age + '\n'
 + 'ваш возраст в днях: ' + (age * 365) + '\n'
 + 'через 5 лет вам будет: ' + (age + 5) + '\n'
 + 'ваш пол: ' + (gender ? "мужской" : "женский")  + '\n'
-+ 'вы на пенсии: ' + ((gender ? age >= 62 : age >= 58) == true? "да" : "нет"));
++ 'вы на пенсии: ' + ((gender ? age >= 62 : age >= 58) == true? "да" : "нет");
+
+alert(information);
 
--- homeworks/homework10/task2/js/anketa.js
+++ homeworks/homework10/task2/js/anketa.js
@@ -5,12 +5,14 @@ var patronymic = prompt('Отчество?');
 var age = parseInt(prompt('Сколько вам лет?', 18));
 var gender = confirm('Bаш пол - мужской??');
 
-alert('ваше ФИО: ' + lastName.trim() + ' ' + firstName.trim() + ' ' + patronymic.trim() + '\n'
+var information = 'ваше ФИО: ' + lastName.trim() + ' ' + firstName.trim() + ' ' + patronymic.trim() + '\n'
 + 'ваш возраст в годах: ' + age + '\n'
 + 'ваш возраст в днях: ' + (age * 365) + '\n'
 + 'через 5 лет вам будет: ' + (age + 5) + '\n'
 + 'ваш пол: ' + getGender(gender) + '\n'
-+ 'вы на пенсии: ' + ((gender ? age >= 62 : age >= 58) == true? "да" : "нет"));
++ 'вы на пенсии: ' + ((gender ? age >= 62 : age >= 58) == true? "да" : "нет");
+
+alert(information);
 
 function getGender(gender) {
     if(gender) {
